1cf6a0b59acdf74a338effeb642fc128c152ffe5
Mohab <124764172+D3vMohab@users.noreply.github.com>
2024-07-08 06:48:28 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/D3vMohab/fastlane-plugin-retrieve-devices.git
+cd fastlane-plugin-retrieve-devices
+fastlane add_plugin retrieve_devices
+
+# test installation
+fastlane retrieve_devices --help
